803dbe061a5166a3ef0d05fb13fb24c22b1162ae
Shallon Kobusinge <50081035+shallonkobusinge@users.noreply.github.com>
2024-08-11 06:22:18 +0200

    feat: initial chat bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,51 @@
 "use client"
-import { Box } from "@mui/material";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 export default function Home() {
   const [messages, setMessages] = useState([
     {role: 'assistant', content: 'Hello and welcome to Visit Rwanda! I’m your AI customer support assistant, How can I assist you today?P'}
   ])
+  const [message, setMessage] = useState('');
+
+  const sendMessage = async () => {
+    setMessage('')
+    setMessages((messages) => [...messages, {role: 'user', content: message}])
+    const response = await fetch("/api/chat",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([...messages, {role: 'user', content: message}]),
+    })
+    const data = await response.json()
+    setMessages((messages) => [...messages, {role:'assistant', content: data.message}])
+  }
+
   return (
     <Box
     width="100vw"
     height="100vh"
     display="flex"
     flexDirection="column"
-    justifyContent="flex-end"
+    justifyContent="center"
+    alignItems="center"
     >
-
+      <Stack direction={'column'} width="500px" maxHeight="700px" border="1px solid black" p={2}>
+        <Stack direction={'column'} spacing={2} flexGrow={1} overflow={'auto'} maxHeight="100%">
+          {messages.map((message, index)=>(
+            <Box key={index} display='flex' justifyContent={message.role === 'assistant'?'flex-start':'flex-end'}>
+              <Box bgcolor={message.role === 'assistant'?'primary.main':'secondary.main'} color="white" borderRadius={16} p={2}>
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <TextField label="Message" fullWidth value={message} 
+          onChange={(e) => setMessage(e.target.value)}/>
+          <Button variant="contained" onClick={sendMessage}>Send</Button>
+        </Stack>
+      </Stack>
     </Box>
   );
 }
